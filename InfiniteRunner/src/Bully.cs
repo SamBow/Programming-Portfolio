e4e680d556b2928e11fb421b0094eafbe18ea935
Sam Cookson <sbcskyline18@gmail.com>
2017-11-09 20:49:15 -0700

    Add files via upload

---
+++ InfiniteRunner/src/Bully.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bully : MonoBehaviour {
+
+	private float moveSpeed = 12f;
+
+	private Rigidbody2D rigid;
+	
+	void Update () {
+
+		gameObject.transform.position = new Vector2(transform.position.x + .20f, transform.position.y);
+	}
+}
